92152056e22b7f0526083ccab7017028fc8dc355
herveyyy <captainhervey@gmail.com>
2024-07-01 02:01:27 +0800

    hehe

--- components/ProjectsComponents/ProjectList.jsx
+++ components/ProjectsComponents/ProjectList.jsx
@@ -1,11 +1,25 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ProjectCard from "./ProjectCard";
+import {
+  FaArrowAltCircleLeft,
+  FaArrowAltCircleRight,
+  FaArrowCircleRight,
+} from "react-icons/fa";
 
 const ProjectList = ({ projects, itemsPerPage = 3 }) => {
-  const responsiveItemsPerPage = window.innerWidth < 1024 ? 1 : itemsPerPage;
+  const [responsiveItemsPerPage, setResponsiveItemsPerPage] =
+    useState(itemsPerPage);
 
+  useEffect(() => {
+    const handleResize = () => {
+      setResponsiveItemsPerPage(window.innerWidth < 1024 ? 1 : itemsPerPage);
+    };
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize); // Cleanup function
+  }, [itemsPerPage]); // Run only when itemsPerPage changes
   const [currentPage, setCurrentPage] = useState(1);
   const handlePageChange = (pageNumber) => {
     setCurrentPage(pageNumber);
@@ -28,7 +42,7 @@ const ProjectList = ({ projects, itemsPerPage = 3 }) => {
   );
 
   return (
-    <div>
+    <div className="  ">
       <div className="  flex flex-col gap-6  laptop:flex-row justify-center laptop:justify-start px-2 ">
         {paginatedProjects.map((project) => (
           <ProjectCard
@@ -42,26 +56,31 @@ const ProjectList = ({ projects, itemsPerPage = 3 }) => {
         ))}
       </div>
 
-      <div className="flex justify-center items-center mt-4">
+      <div className="flex justify-center gap-x-4 items-center mt-4 ">
         <button
+          className={`justify-center cursor-pointer  p-2 gap-2 items-center translate-x-0 hover:-translate-x-1 duration-700 flex`}
           onClick={() => handlePageChange(currentPage - 1)}
           disabled={currentPage === 1}
         >
-          Previous
+          <FaArrowAltCircleLeft className="h-5 w-5" />
+          Prev
         </button>
-        <span>
-          Page {currentPage} of{" "}
-          {Math.ceil(projects.length / responsiveItemsPerPage)}
-        </span>
+
         <button
+          className="flex justify-center p-2 gap-2  cursor-pointer items-center translate-x-0 hover:translate-x-1 duration-700"
           onClick={() => handlePageChange(currentPage + 1)}
           disabled={
             currentPage === Math.ceil(projects.length / responsiveItemsPerPage)
           }
         >
           Next
+          <FaArrowAltCircleRight className="h-5 w-5 " />
         </button>
       </div>
+      <p className="  text-center w-full">
+        Page {currentPage} of{" "}
+        {Math.ceil(projects.length / responsiveItemsPerPage)}
+      </p>
     </div>
   );
 };
